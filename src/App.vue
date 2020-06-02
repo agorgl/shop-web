d2bfa9b6eeb8f3f89a62a2323bc2792e243975b2
TheArtist <agorglouk@gmail.com>
2020-06-02 16:42:20 +0300

    Implemented responsive sidebar/content transition

--- src/App.vue
+++ src/App.vue
@@ -77,8 +77,8 @@ export default {
 #app {
   height: 100vh;
   display: grid;
-  grid-template-rows: min-content auto;
-  grid-template-columns: min-content auto;
+  grid-template-rows: min-content 1fr;
+  grid-template-columns: min-content 1fr;
   grid-template-areas: 'logo header' 'sidebar content';
   overflow: hidden;
   font-family: Avenir, Helvetica, Arial, sans-serif;
@@ -91,8 +91,8 @@ export default {
   display: flex;
   align-items: center;
   justify-content: center;
-  grid-area: logo;
   box-shadow: 0 1px 2px 0 rgba(34,36,38,.15);
+  grid-area: logo;
 }
 
 #logo img {
@@ -133,6 +133,7 @@ export default {
 }
 
 #content {
+  width: 100%;
   padding: 1rem;
   display: block;
   overflow: auto;
@@ -145,5 +146,26 @@ export default {
 
 .content-anim-enter, .content-anim-leave-to {
   opacity: 0;
+}
+
+#sidebar[style*="display: none;"] + #content,
+#sidebar[class~="sidebar-anim-enter-active"] + #content {
+  grid-column-start: 1;
+}
+
+#sidebar[class~="sidebar-anim-enter-active"] + #content,
+#sidebar[class~="sidebar-anim-leave-active"] + #content {
+  transition: width 0.3s ease,
+              transform 0.3s ease;
+}
+
+#sidebar[class~="sidebar-anim-enter-to"] + #content {
+  width: calc(100% - 64px);
+  transform: translateX(64px);
+}
+
+#sidebar[class~="sidebar-anim-leave-to"] + #content {
+  width: calc(100% + 64px);
+  transform: translateX(-64px);
 }
 </style>
